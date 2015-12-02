3304a39440d1e954f9e3140143ca4c9044d74c01
Brian Carrigan <brian.carrigan@smashingboxes.com>
2015-12-02 09:18:02 -0500

    Spiral example running.

--- src/storefront/core.clj
+++ src/storefront/core.clj
@@ -1,50 +1,50 @@
 (ns storefront.core
   (:gen-class)
   (:require [quil.core :as q]
-            [quil.middleware :as m]))
+            [quil.middleware :as m]
+            [quil.helpers.drawing :refer [line-join-points]]
+            [quil.helpers.seqs :refer [range-incl steps]]
+            [quil.helpers.calc :refer [mul-add]]))
+
+(defn noisify [sample]
+  (* 100 (q/noise sample)))
 
 (defn setup []
-  ; Set frame rate to 30 frames per second.
   (q/frame-rate 30)
-  ; Set color mode to HSB (HSV) instead of default RGB.
-  (q/color-mode :hsb)
-  ; setup function returns initial state. It contains
-  ; circle color and position.
-  {:color 0
-   :angle 0})
+  (q/background 255)
+  (q/stroke-weight 5)
+  (q/smooth)
+  (let [radius    100
+        cent-x    250
+        cent-y    250
+        rad-noise (steps (rand 10) 0.05)
+        rad-noise (map noisify rad-noise)
+        rads      (map q/radians (range-incl 0 1440 5))
+        radii     (steps 10 0.5)
+        radii     (map (fn [rad noise] (+ rad noise -100)) radii rad-noise)
+        xs        (map (fn [rad radius] (mul-add (q/cos rad) radius cent-x)) rads radii)
+        ys        (map (fn [rad radius] (mul-add (q/sin rad) radius cent-y)) rads radii)
+        line-args (line-join-points xs ys)]
+    (q/stroke 0 30)
+    (q/no-fill)
+    (q/ellipse cent-x cent-y (* radius 2) (* radius 2))
+    (q/stroke 20 50 70)
+    (dorun (map #(apply q/line %) line-args)))
+
+  { })
 
 (defn update-state [state]
-  ; Update sketch state by changing circle color and position.
-  {:color (mod (+ (:color state) 0.7) 255)
-   :angle (+ (:angle state) 0.1)})
+  { })
 
-(defn draw-state [state]
-  ; Clear the sketch by filling it with light-grey color.
-  (q/background 240)
-  ; Set circle color.
-  (q/fill (:color state) 255 255)
-  ; Calculate x and y coordinates of the circle.
-  (let [angle (:angle state)
-        x (* 150 (q/cos angle))
-        y (* 150 (q/sin angle))]
-    ; Move origin point to the center of the sketch.
-    (q/with-translation [(/ (q/width) 2)
-                         (/ (q/height) 2)]
-      ; Draw the circle.
-      (q/ellipse x y 100 100))))
+(defn draw-state [state])
 
 (q/defsketch storefront
-  :title "You spin my circle right round"
+  :title "Spiral Demo"
   :size [500 500]
-  ; setup function called only once, during sketch initialization.
   :setup setup
-  ; update-state is called on each iteration before draw-state.
   :update update-state
   :draw draw-state
   :features [:keep-on-top]
-  ; This sketch uses functional-mode middleware.
-  ; Check quil wiki for more info about middlewares and particularly
-  ; fun-mode.
   :middleware [m/fun-mode])
 
 (defn -main [& args])
