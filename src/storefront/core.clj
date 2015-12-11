ee14d4549224275fce4ed447b0733142c364661f
Brian Carrigan <brian.carrigan@smashingboxes.com>
2015-12-11 10:23:17 -0500

    Added drawing compartmentalization.

--- src/storefront/core.clj
+++ src/storefront/core.clj
@@ -1,51 +1,21 @@
 (ns storefront.core
   (:gen-class)
-  (:require [storefront.scanlines :as scanlines]
-            [storefront.spiral :as spiral]
-            [storefront.pulsar :as pulsar]
-            [storefront.arduino :as arduino]
-            [quil.core :as q]
+  (:require [quil.core :as q]
             [quil.middleware :as m]
-            [serial-port :as ser]))
+            [storefront.spiral-glitch :as spiral]))
 
-(def noise-jitter 300)
-(def update-interval 1000)
+(def drawing spiral/drawing)
 
-(defn setup
-  []
-  (q/frame-rate 30)
-  { :arduino  (arduino/initialize "/dev/tty.usbmodem453061")
-    :scan     (scanlines/initialize 2 0.2)
-    :pulsar   (pulsar/initialize update-interval)
-    :spiral   (spiral/initialize noise-jitter)
-    :spiral-2 (spiral/initialize noise-jitter) })
+(defn load-drawing
+  [drawing-info]
+  (q/defsketch storefront
+    :title "Twitchy Spiral"
+    :size   :fullscreen
+    :setup  (:setup drawing-info)
+    :update (:update-state drawing-info)
+    :draw   (:draw-state drawing-info)
+    :features [:keep-on-top :present]
+    :middleware [m/fun-mode]))
 
-(defn update-state
-  [state]
-  (let [updated-pulsar   (pulsar/update (:pulsar state))
-        ; pulse          (pulsar/fired updated-pulsar)
-        updated-arduino  (arduino/update (:arduino state))
-        pulse            (:fired updated-arduino)]
-    { :arduino  updated-arduino
-      :scan     (scanlines/update (:scan state) pulse)
-      :pulsar   updated-pulsar
-      :spiral   (spiral/update (:spiral state) pulse)
-      :spiral-2 (spiral/update (:spiral-2 state) pulse) }))
-
-(defn draw-state
-  [state]
-  (q/background 255)
-  (spiral/draw (:spiral state))
-  (spiral/draw (:spiral-2 state))
-  (scanlines/draw (:scan state)))
-
-(q/defsketch storefront
-  :title "Twitchy Spiral"
-  :size :fullscreen
-  :setup setup
-  :update update-state
-  :draw draw-state
-  :features [:keep-on-top :present]
-  :middleware [m/fun-mode])
-
+(load-drawing drawing)
 (defn -main [& args])
---
+++ src/storefront/spiral_glitch.clj
@@ -0,0 +1,44 @@
+(ns storefront.spiral-glitch
+  (:gen-class)
+  (:require [storefront.scanlines :as scanlines]
+            [storefront.spiral :as spiral]
+            [storefront.pulsar :as pulsar]
+            [storefront.arduino :as arduino]
+            [quil.core :as q]))
+
+(def noise-jitter 300)
+(def update-interval 1000)
+
+(defn setup
+  []
+  (q/frame-rate 30)
+  { :arduino  (arduino/initialize "/dev/tty.usbmodem453061")
+    :scan     (scanlines/initialize 2 0.2)
+    :pulsar   (pulsar/initialize update-interval)
+    :spiral   (spiral/initialize noise-jitter)
+    :spiral-2 (spiral/initialize noise-jitter) })
+
+(defn update-state
+  [state]
+  (let [updated-pulsar   (pulsar/update (:pulsar state))
+        ; pulse          (pulsar/fired updated-pulsar)
+        updated-arduino  (arduino/update (:arduino state))
+        pulse            (:fired updated-arduino)]
+    { :arduino  updated-arduino
+      :scan     (scanlines/update (:scan state) pulse)
+      :pulsar   updated-pulsar
+      :spiral   (spiral/update (:spiral state) pulse)
+      :spiral-2 (spiral/update (:spiral-2 state) pulse) }))
+
+(defn draw-state
+  [state]
+  (q/background 255)
+  (spiral/draw (:spiral state))
+  (spiral/draw (:spiral-2 state))
+  (scanlines/draw (:scan state)))
+
+(def drawing {
+  :title "Twitchy Spiral"
+  :setup setup
+  :update-state update-state
+  :draw-state draw-state })
