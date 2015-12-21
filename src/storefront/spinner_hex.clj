4bbb4989a95d0a6176a52b7ad851094cd1bf4434
Derek Kniffin <derek.kniffin@gmail.com>
2015-12-21 11:57:52 -0500

    Separated creation of triangles from draw

--- src/storefront/spinner_hex.clj
+++ src/storefront/spinner_hex.clj
@@ -4,44 +4,44 @@
             [quil.core :as q])
   (:import  [storefront.drawing Drawing]))
 
-(defn draw-tri-hex [x y radius spacing]
-  (q/push-matrix)
-  (q/translate x y)
-  ;; (doseq [angle (map #(* (/ q/PI 3) %) (range 6))]
-    ;; (q/push-matrix)
-    ;; (q/vertex (* (q/sin angle) radius) (* (q/cos angle) radius))
-    ;; (q/pop-matrix))
+(defrecord Triangle [x1 y1 x2 y2 x3 y3])
+
+(defn draw-triangle [triangle x y]
+  (let [coords (map-indexed (fn [i val] (if (even? i) (+ val x) (+ val y))) (vals triangle))]
+    (apply q/triangle coords)))
+
+(defn hex-triangles [radius]
   (let [angles (map #(* (/ q/PI 3) %) (range 6))
         pairs  (map-indexed
                   (fn [i angle]
                     (let [next-angle (nth angles (mod (+ i 1) (count angles)))]
                     [angle next-angle]))
                   angles)]
-        (doseq [pair pairs]
-          (let [angle      (get pair 0)
-                next-angle (get pair 1)]
-            (q/begin-shape)
-            (q/push-matrix)
-            (q/vertex 0 0)
-            (q/vertex (* (q/sin angle) radius) (* (q/cos angle) radius))
-            (q/vertex (* (q/sin next-angle) radius) (* (q/cos next-angle) radius))
-            (q/pop-matrix)
-            (q/end-shape :close)
-          )
-        )
-  )
+        (map
+          #(let [[angle next-angle] %]
+            (Triangle.
+              0
+              0
+              (* (q/sin angle) radius)
+              (* (q/cos angle) radius)
+              (* (q/sin next-angle) radius)
+              (* (q/cos next-angle) radius)
+            ))
+          pairs)))
 
-  (q/pop-matrix))
+(defn setup []
+  { :triangles (hex-triangles 200)})
 
-
-(defn setup [] )
-
-(defn update-state [state] )
+(defn update-state [state]
+  state)
 
 (defn draw-state [state]
   (q/background 0 0 0)
   (q/stroke-weight 1)
   (q/stroke 255 255 255)
-  (draw-tri-hex (/ (q/width) 2) (/ (q/height) 2) 200 0))
+  (let [x (/ (q/width) 2)
+        y (/ (q/height) 2)]
+    (doseq [triangle (:triangles state)]
+      (draw-triangle triangle x y))))
 
 (def drawing (Drawing. "Spinner Hex" setup update-state draw-state :fullscreen [:keep-on-top :present]))
