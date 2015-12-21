2ff356ed1f0bce0ed3f36293d6341c5757faa0f8
Derek Kniffin <derek.kniffin@gmail.com>
2015-12-21 13:12:32 -0500

    Make the spinner spin

--- src/storefront/spinner_hex.clj
+++ src/storefront/spinner_hex.clj
@@ -1,29 +1,28 @@
 (ns storefront.spinner-hex
   (:require [storefront.drawing]
             [storefront.helpers :refer :all]
-            [quil.core :as q])
+            [quil.core :as q]
+            [clojure.core.matrix :as m])
   (:import  [storefront.drawing Drawing]))
 
-(defrecord Triangle [x1 y1 x2 y2 x3 y3 color])
+(defrecord Triangle [x1 y1 x2 y2 x3 y3])
 
-(defn draw-triangle [triangle x y]
+(defn draw-triangle [triangle x y color]
   (let [x1 (+ (:x1 triangle) x)
         y1 (+ (:y1 triangle) y)
         x2 (+ (:x2 triangle) x)
         y2 (+ (:y2 triangle) y)
         x3 (+ (:x3 triangle) x)
         y3 (+ (:y3 triangle) y)]
-    (q/fill (:color triangle))
+    (q/fill color)
     (q/triangle x1 y1 x2 y2 x3 y3)))
 
 (defn hex-triangles [radius]
   (let [angles (map #(* (/ q/PI 3) %) (range 6))
-        spinner-color (q/color 235 23 103) ;; SB pink
-        colors  (map #(q/lerp-color (q/color 0 0 0) spinner-color (/ % 6)) (range 6))
         sets  (map-indexed
                   (fn [i angle]
                     (let [next-angle (nth angles (mod (+ i 1) (count angles)))]
-                    [angle next-angle (nth colors i)]))
+                    [angle next-angle]))
                   angles)
         ]
         (map
@@ -35,22 +34,27 @@
               (* (q/cos angle) radius)
               (* (q/sin next-angle) radius)
               (* (q/cos next-angle) radius)
-              color
             ))
-          sets)))
+          (reverse sets))))
 
 (defn setup []
-  { :triangles (hex-triangles 200)})
+  { :triangles (hex-triangles 200) })
 
 (defn update-state [state]
-  state)
+  (update-in state [:triangles] #(m/rotate % 0 1)))
 
 (defn draw-state [state]
+  (q/frame-rate 10)
   (q/background 0 0 0)
   (q/no-stroke)
   (let [x (/ (q/width) 2)
-        y (/ (q/height) 2)]
-    (doseq [triangle (:triangles state)]
-      (draw-triangle triangle x y))))
+        y (/ (q/height) 2)
+        spinner-color (q/color 235 23 103) ;; SB pink
+        colors (map #(q/lerp-color (q/color 0 0 0) spinner-color (/ % 6)) (range 6))]
+    (dorun
+      (map (fn [triangle color]
+          (draw-triangle triangle x y color))
+        (:triangles state)
+        colors))))
 
 (def drawing (Drawing. "Spinner Hex" setup update-state draw-state :fullscreen [:keep-on-top :present]))
