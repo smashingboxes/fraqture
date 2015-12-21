a4824ea27e1b1552886522904e54bb9370be104f
Derek Kniffin <derek.kniffin@gmail.com>
2015-12-21 12:17:21 -0500

    Colored the triangles

--- src/storefront/spinner_hex.clj
+++ src/storefront/spinner_hex.clj
@@ -4,21 +4,30 @@
             [quil.core :as q])
   (:import  [storefront.drawing Drawing]))
 
-(defrecord Triangle [x1 y1 x2 y2 x3 y3])
+(defrecord Triangle [x1 y1 x2 y2 x3 y3 color])
 
 (defn draw-triangle [triangle x y]
-  (let [coords (map-indexed (fn [i val] (if (even? i) (+ val x) (+ val y))) (vals triangle))]
-    (apply q/triangle coords)))
+  (let [x1 (+ (:x1 triangle) x)
+        y1 (+ (:y1 triangle) y)
+        x2 (+ (:x2 triangle) x)
+        y2 (+ (:y2 triangle) y)
+        x3 (+ (:x3 triangle) x)
+        y3 (+ (:y3 triangle) y)]
+    (q/fill (:color triangle))
+    (q/triangle x1 y1 x2 y2 x3 y3)))
 
 (defn hex-triangles [radius]
   (let [angles (map #(* (/ q/PI 3) %) (range 6))
-        pairs  (map-indexed
+        spinner-color (q/color 235 23 103) ;; SB pink
+        colors  (map #(q/lerp-color (q/color 0 0 0) spinner-color (/ % 6)) (range 6))
+        sets  (map-indexed
                   (fn [i angle]
                     (let [next-angle (nth angles (mod (+ i 1) (count angles)))]
-                    [angle next-angle]))
-                  angles)]
+                    [angle next-angle (nth colors i)]))
+                  angles)
+        ]
         (map
-          #(let [[angle next-angle] %]
+          #(let [[angle next-angle color] %]
             (Triangle.
               0
               0
@@ -26,8 +35,9 @@
               (* (q/cos angle) radius)
               (* (q/sin next-angle) radius)
               (* (q/cos next-angle) radius)
+              color
             ))
-          pairs)))
+          sets)))
 
 (defn setup []
   { :triangles (hex-triangles 200)})
@@ -37,8 +47,7 @@
 
 (defn draw-state [state]
   (q/background 0 0 0)
-  (q/stroke-weight 1)
-  (q/stroke 255 255 255)
+  (q/no-stroke)
   (let [x (/ (q/width) 2)
         y (/ (q/height) 2)]
     (doseq [triangle (:triangles state)]
