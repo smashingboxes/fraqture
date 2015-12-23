b848147c3569789d755f60fc860c0a2cfeef3771
Derek Kniffin <derek.kniffin@gmail.com>
2015-12-23 17:48:49 -0500

    Got argument parsing and validation working

--- src/storefront/core.clj
+++ src/storefront/core.clj
@@ -2,6 +2,8 @@
   (:gen-class)
   (:require [quil.core :as q]
             [quil.middleware :as m]
+            [clojure.string :as string]
+            [clojure.tools.cli :refer [parse-opts]]
             [storefront.cycle :as cycle]
             [storefront.glitch-drag :as drag]
             [storefront.spirograph :as spirograph]
@@ -12,29 +14,40 @@
             [storefront.weather-drawing :as weather]
             ))
 
-(defn load-drawing
-  [drawing-info]
-  (q/defsketch storefront
-    :title  (:title drawing-info)
-    :size   (:size drawing-info)
-    :setup  (:setup-fn drawing-info)
-    :update (:update-fn drawing-info)
-    :draw   (:draw-fn drawing-info)
-    :features (:features drawing-info)
-    :middleware [m/fun-mode]))
+(defn load-drawing [drawing args]
+  (let [args-hash (parse-opts args (:cli-options-fn drawing))
+        errors    (:errors args-hash)
+        ]
+    (if errors
+      (dorun
+        (println (string/join "\n" errors))
+        (System/exit 1))
+      (q/defsketch storefront
+        :title  (:title drawing)
+        :size   (:size drawing)
+        :setup  (:setup-fn drawing)
+        :update (:update-fn drawing)
+        :draw   (:draw-fn drawing)
+        :features (:features drawing)
+        :middleware [m/fun-mode]))))
+
+(def drawing-hash (hash-map
+    "spiro"         spirograph/drawing
+    "drag"          drag/drawing
+    "spiro"         spirograph/drawing
+    "shifting-grid" shifting-grid/drawing
+    "hex"           hexagons/drawing
+    "hex-spinner"   hex-spinner/drawing
+    "cycle"         cycle/drawing
+    "pixelate"      pixelate/drawing
+    "weather"       weather/drawing
+  ))
+
+(def basic-usage
+  (str "lein run <drawing> [args]\n drawings: " (keys drawing-hash)))
 
 (defn -main [& args]
-  (let [name (nth args 0)
-        drawings (hash-map
-          "spiro"         spirograph/drawing
-          "drag"          drag/drawing
-          "spiro"         spirograph/drawing
-          "shifting-grid" shifting-grid/drawing
-          "hex"           hexagons/drawing
-          "hex-spinner"   hex-spinner/drawing
-          "cycle"         cycle/drawing
-          "pixelate"      pixelate/drawing
-          "weather"       weather/drawing
-        )]
-    (load-drawing (get drawings name))
-  ))
+  (let [[drawing-name & drawing-args] args]
+    (if (not (contains? (set (keys drawing-hash)) drawing-name))
+      (println basic-usage)
+      (load-drawing (get drawing-hash drawing-name) drawing-args))))
--- src/storefront/shifting_grid.clj
+++ src/storefront/shifting_grid.clj
@@ -5,73 +5,84 @@
             [clojure.core.matrix :as m])
   (:import  [storefront.drawing Drawing]))
 
-(def x-blocks 50)
-(def y-blocks 30)
-(def max-chunk-size 10)
-(def max-rotation-length 3)
-
-(def block-width #(/ (q/width) x-blocks))
-(def block-height #(/ (q/height) y-blocks))
-(def total-blocks (* x-blocks y-blocks))
-
-
-
-(defn draw-block [block x-index y-index]
-  (let [x                 (* (block-width) x-index)
-        y                 (* (block-height) y-index)]
-    (q/rect x y (block-width) (block-height))
-    (q/image block x y (block-width) (block-height))))
-
-(defn get-block [image x y]
-  (q/get-pixel image x y (block-width) (block-height)))
+(defn draw-block [block x-index y-index w h]
+  (let [x                 (* w x-index)
+        y                 (* h y-index)]
+    (q/rect x y w h)
+    (q/image block x y w h)))
 
 (defn rand-speed [min-max]
   ((rand-nth [- +]) 0 (+ (rand-int min-max) 1)))
 
-(defn rotate-nth [matrix n]
-  (let [shift (rand-speed max-rotation-length)]
+(defn rotate-nth [matrix n max-rotation]
+  (let [shift (rand-speed max-rotation)]
     (update-in matrix [n] #(m/rotate % 0 shift))))
 
 ; Returns n indices, from start, and wrapping after max
 (defn n-indices-wrapped [n start y]
   (map #(mod % y) (map #(+ start %) (range n))))
 
-(defn matrix-rotate-generator [n column?]
-  (fn [matrix]
-    (if column?
-      (rotate-nth matrix n)
-      (m/transpose (rotate-nth (m/transpose matrix) n)))))
-
-
-(defn random-rotation [matrix]
+(defn random-rotation [matrix max-rotation chunk-size]
   (let [column? (rand-nth '(true false))
+        x-blocks (count matrix)
+        y-blocks (count (first matrix))
         max     (if column? (- x-blocks 1) (- y-blocks 1))
         start   (rand-int max)
-        size    (+ (rand-int max-chunk-size) 1)
+        size    chunk-size
         columns (n-indices-wrapped size start max)
         rotated (if column? matrix (m/transpose matrix))
-        shifted (reduce rotate-nth rotated columns)]
+        shifted (reduce rotate-nth rotated columns max-rotation)]
     (if column? shifted (m/transpose shifted))))
 
-(defn setup []
+(def cli-options
+  [
+    ["-x" "--x-blocks INT" "Number of blocks in the horizontal"
+      :default 50
+      :parse-fn #(Integer/parseInt %)
+      :validate [#(< 2 % 200) "Must be a number between 2 and 200"]]
+    ["-y" "--y-blocks INT" "Number of blocks in the vertical direction"
+      :default 30
+      :parse-fn #(Integer/parseInt %)
+      :validate [#(< 2 % 200) "Must be a number between 0 and 200"]]
+    ["-c" "--chunk-size INT" "Number of columns/rows to slide together"
+      :default 5
+      :parse-fn #(Integer/parseInt %)
+      :validate [#(< 1 % 10) "Must be a number between 1 and 10"]]
+    ["-r" "--max-rotation INT" "Max number of positions to rotate columns/rows"
+      :default 3
+      :parse-fn #(Integer/parseInt %)
+      :validate [#(< 1 % 10) "Must be a number between 1 and 10"]]
+  ])
+
+(defn setup [options]
   (q/frame-rate 5)
   (let [image-file (random-image-file)
         image  (q/load-image image-file)
         _resized (q/resize image (q/width) (q/height))
-        xs     (map #(* % (block-width)) (range x-blocks))
-        ys     (map #(* % (block-height))  (range y-blocks))
-        blocks (map (fn [x] (map (fn [y] (get-block image x y)) ys)) xs)]
-        { :blocks blocks }))
+        x-blocks (:x-blocks options)
+        y-blocks (:y-blocks options)
+        block-w (/ (q/width) x-blocks)
+        block-h (/ (q/height) y-blocks)
+        xs     (map #(* % block-w) (range x-blocks))
+        ys     (map #(* % block-h) (range y-blocks))
+        blocks (map (fn [x] (map (fn [y] (q/get-pixel image x y block-w block-h)) ys)) xs)]
+        { :blocks blocks
+          :options options
+          :block-w block-w
+          :block-h block-h }))
 
 (defn update-state [state]
-  (update-in state [:blocks] random-rotation))
+  (let [options (:options state)
+        max-rotation (:max-rotation options)
+        chunk-size (:chunk-size options)]
+    (update-in state [:blocks] #(random-rotation % max-rotation chunk-size))))
 
 (defn draw-state [state]
   (dorun
     (map-indexed (fn [x-index column]
       (dorun
         (map-indexed (fn [y-index block]
-          (draw-block block x-index y-index))
+          (draw-block block x-index y-index (:block-w state) (:block-h state)))
         column)))
     (:blocks state))))
 
