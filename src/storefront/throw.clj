da56a88ff3ffa6c0d4dcafbdd50c90b89bc54fd0
Brian Carrigan <brian.carrigan@smashingboxes.com>
2016-01-22 15:20:35 -0500

    Synced a circle with the animation.

--- src/storefront/throw.clj
+++ src/storefront/throw.clj
@@ -6,20 +6,31 @@
   (:import  [storefront.drawing Drawing]))
 
 (def cli-options [["-p" "--port PATH" "Serial port, /dev/cu.*"]])
+(def csize 80)
+
+(defn fire-leds [state]
+  (ser/write (:port state) (byte-array 1)))
 
 (defn setup [options]
   (let [serial    (ser/open (:port options))]
-    (q/background 0)
-    { :port serial
-      :next-fire (q/millis)
-      :fired false }))
+    { :loc (+ csize (q/width))
+      :fresh true
+      :port serial
+      :next-fire (q/millis) }))
 
 (defn update-state [state]
   (if (> (q/millis) (:next-fire state))
-    (assoc state :next-fire (+ (q/millis) 15000) :fired true)
-    (assoc state :fired false)))
+    (assoc state :loc (+ csize (q/width)) :next-fire (+ (q/millis) 6000) :fresh true)
+    (let [new-loc (- (:loc state) 10)]
+      (-> (if (and (:fresh state) (< new-loc 0))
+            (do (fire-leds state)
+                (assoc state :fresh false))
+            state)
+          (assoc :loc new-loc)))))
 
 (defn draw-state [state]
-  (if (:fired state) (ser/write (:port state) (byte-array 1))))
+  (q/background 0)
+  (q/fill 255)
+  (q/ellipse (:loc state) (/ (q/height) 2) csize csize))
 
 (def drawing (Drawing. "throw" setup update-state draw-state cli-options nil))
