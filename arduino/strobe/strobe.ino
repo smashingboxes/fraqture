ea9f6091803f9828032c2c8a3557bfdee9e2a0e3
Carrigan <brian.c.carrigan@gmail.com>
2016-02-20 18:20:07 -0500

    Began adding a terminal to the storefront LED array.

--- arduino/strobe/strobe.ino
+++ arduino/strobe/strobe.ino
@@ -1,30 +1,76 @@
 #include <SPI.h>
 #include "led_strip.h"
+#include "terminal.h"
 
+terminal_t terminal;
 led_strip_t strip;
-uint8_t current = 0;
 
-void led_transfer(uint8_t x) {
-  SPI.transfer(x);
-  Serial.begin(9600);
-}
+typedef struct {
+  uint16_t index;
+  color_t color;
+} set_packet_t;
 
 void clear_led_f(uint16_t index, color_t *color)
 {
   memset(color, 0, sizeof(color_t));
 }
 
-void clear_leds(void) { led_map(&strip, clear_led_f); }
+void clear_leds(void *_none)
+{
+  led_map(&strip, clear_led_f);
+}
+
+void window_leds(void *window)
+{
+  window_t cast_window = (window_t *)window;
+  led_window(&strip, window);
+}
+
+void set_leds(void *set)
+{
+  set_packet_t cast_set = (set_packet_t *)set;
+  led_set(&strip, set->index, &set->color);
+}
+
+terminal_cmd_t cmd_clear = {
+  .trigger = 'C',
+  .length = 0,
+  .handler = clear_leds,
+  .next = NULL
+};
+
+terminal_cmd_t cmd_window = {
+  .trigger = 'W',
+  .length = sizeof(window_t),
+  .handler = window_leds,
+  .next = NULL
+};
+
+terminal_cmd_t cmd_set = {
+  .trigger = 'S',
+  .length = sizeof(set_packet_t),
+  .handler = set_leds,
+  .next = NULL
+};
+
+void led_transfer(uint8_t x) {
+  SPI.transfer(x);
+  Serial.begin(9600);
+}
 
 void setup() {
   SPI.begin();
   led_init(&strip, led_transfer);
-  clear_leds();
+  clear_leds(NULL);
+  terminal_init(&terminal);
+  terminal_attach(&terminal, &cmd_clear);
+  terminal_attach(&terminal, &cmd_window);
+  terminal_attach(&terminal, &cmd_set);
 }
 
 void loop() {
   if(Serial.available()) {
-    // Add commands here
+    terminal_feed(&terminal, Serial.read());
     led_refresh(&strip);
   }
 }
---
+++ arduino/strobe/terminal.h
@@ -0,0 +1,38 @@
+#ifndef TERMINAL_H_
+#define TERMINAL_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#define CMD_LENGTH   5
+
+typedef void (*terminal_handler)(void *);
+
+typedef struct terminal_cmd {
+  char trigger;
+  uint8_t length;
+  terminal_handler handler;
+  struct terminal_cmd *next;
+} terminal_cmd_t;
+
+typedef struct {
+  terminal_cmd_t *current_handler;
+  terminal_cmd_t *handler_chain;
+  uint8_t character_index;
+  uint8_t reset_index;
+  char buffer[CMD_LENGTH];
+} terminal_t;
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+void terminal_init(terminal_t *term);
+bool terminal_attach(terminal_t *term, terminal_cmd_t *cmd);
+void terminal_feed(terminal_t *term, char incoming);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
