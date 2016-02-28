a29c835881d54cb1471f25fe06a16a0046d496d6
David Sweetman <david@davidsweetman.com>
2016-02-28 10:41:02 -0500

    Move the ripple state to a struct. Hide the struct declaration and just distribute a malloc'd pointer to it on ripple_init. Dynamic memory allocation might be a bad idea on an arduino.

--- arduino/sim/ripple.c
+++ arduino/sim/ripple.c
@@ -1,61 +1,68 @@
 #include "ripple.h"
 #include "../strobe/color.h"
 #include "../strobe/led_strip.h"
-#include "../strobe/led_array.h"
+#include <stdlib.h>
 #include <string.h>
-
-#define HALF_ROW_COUNT (int)(ROW_COUNT / 2)
-
-uint8_t frame_one[HALF_ROW_COUNT][COL_COUNT];
-uint8_t frame_two[HALF_ROW_COUNT][COL_COUNT];
 
 typedef uint8_t(* frame_ptr)[COL_COUNT];
 
-frame_ptr active_frame = frame_one;
-frame_ptr inactive_frame = frame_two;
+typedef struct ripple_state {
+  uint8_t frame_one[HALF_ROW_COUNT][COL_COUNT];
+  uint8_t frame_two[HALF_ROW_COUNT][COL_COUNT];
+  frame_ptr active_frame;
+  frame_ptr inactive_frame;
+} ripple_state_t;
 
 void zero_heightmap(frame_ptr heightmap)
 {
     memset(heightmap, 0, sizeof(heightmap[0][0]) * COL_COUNT * HALF_ROW_COUNT);
 }
 
-void ripple_init()
+ripple_state_t* ripple_init()
 {
-  zero_heightmap(active_frame);
-  zero_heightmap(inactive_frame);
+  ripple_state_t* ripple = (ripple_state_t*)malloc(sizeof(ripple_state_t));
+  zero_heightmap(ripple->active_frame);
+  zero_heightmap(ripple->inactive_frame);
+  return ripple;
 }
 
-void swap_buffers()
+void ripple_release(ripple_state_t* ripple)
 {
-  frame_ptr temp = active_frame;
-  active_frame = inactive_frame;
-  inactive_frame = temp;
+    free(ripple);
+    ripple = NULL;
 }
 
-void ripple_touch_point(float x, float y, float strength)
+void swap_buffers(ripple_state_t* ripple)
+{
+  frame_ptr temp = ripple->active_frame;
+  ripple->active_frame = ripple->inactive_frame;
+  ripple->inactive_frame = temp;
+}
+
+void ripple_touch_point(ripple_state_t* ripple, float x, float y, float strength)
 {
   int x_index = (int)(x * COL_COUNT);
   int y_index = (int)(y * HALF_ROW_COUNT);
-  active_frame[x_index][y_index] = 255 * strength;
+  ripple->active_frame[x_index][y_index] = 255 * strength;
 }
 
-void ripple_update()
+void ripple_update(ripple_state_t* ripple)
 {
   // http://freespace.virgin.net/hugo.elias/graphics/x_water.htm
   float decay = 0.88;
   for (int x = 1; x < COL_COUNT-1; x++) {
     for (int y = 1; y < HALF_ROW_COUNT-1; y++) {
-      uint8_t avg = (active_frame[x-1][y] +
-                     active_frame[x+1][y] +
-                     active_frame[x][y-1] +
-                     active_frame[x][y+1]) / 4;
-      inactive_frame[x][y] = (avg * 2 - inactive_frame[x][y]) * decay;
+      uint8_t avg = (ripple->active_frame[x-1][y] +
+                     ripple->active_frame[x+1][y] +
+                     ripple->active_frame[x][y-1] +
+                     ripple->active_frame[x][y+1]) / 4;
+      ripple->inactive_frame[x][y] = (avg * 2 - ripple->inactive_frame[x][y]) * decay;
     }
   }
-  swap_buffers();
+  swap_buffers(ripple);
 }
 
-void ripple_sim_to_colors(color_t* colors)
+void ripple_sim_to_colors(ripple_state_t* ripple, color_t* colors)
 {
   color_t high = { 200, 200, 255 };
   color_t low = { 0, 0, 0 };
@@ -63,7 +70,7 @@ void ripple_sim_to_colors(color_t* colors)
   for (int x = 0; x < COL_COUNT; x++) {
     for (int y = 0; y < HALF_ROW_COUNT; y++) {
 
-      float amount  = active_frame[x][y] / 255.0f;
+      float amount  = ripple->active_frame[x][y] / 255.0f;
       color_t color;
       lerp_color(&low, &high, amount, &color);
 
--- arduino/sim/ripple.h
+++ arduino/sim/ripple.h
@@ -2,24 +2,40 @@
 #define LIQUID_H_
 
 #include "../strobe/color.h"
+#include "../strobe/led_array.h"
+#define HALF_ROW_COUNT (int)(ROW_COUNT / 2)
 
 #ifdef __cplusplus
 extern "C"{
 #endif
 
-void ripple_init();
-void ripple_update();
+typedef struct ripple_state ripple_state_t;
+
+/**
+ *  Create a new ripple simulation.
+ */
+ripple_state_t* ripple_init();
+
+/**
+ *  Destroy an existing ripple simulation.
+ */
+void ripple_release(ripple_state_t* ripple);
+
+/**
+ *  Step the simulation.
+ */
+void ripple_update(ripple_state_t* ripple);
 
 /**
  * @param x, y The coordinate to touch (normalized to 0..1)
  * @param strength The strength of the disturbance from 0..1
  */
-void ripple_touch_point(float x, float y, float strength);
+void ripple_touch_point(ripple_state_t* ripple, float x, float y, float strength);
 
 /**
  *  @param colors The colors to fill
  */
-void ripple_sim_to_colors(color_t* colors);
+void ripple_sim_to_colors(ripple_state_t* ripple, color_t* colors);
 
 #ifdef __cplusplus
 }
