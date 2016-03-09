172649f7e3ebef9ddd4ef3e88e42ec43a8d8dc60
Brian Carrigan <brian.carrigan@smashingboxes.com>
2016-03-09 11:46:29 -0500

    Fixed LED indexing and communication byte ordering.

--- arduino/strobe/color.h
+++ arduino/strobe/color.h
@@ -3,10 +3,11 @@
 
 #include <stdint.h>
 
-typedef struct {
-  uint8_t r;
-  uint8_t g;
+typedef struct __attribute__((__packed__)){
+
   uint8_t b;
+  uint8_t g;
+  uint8_t r;
 } color_t;
 
 #ifdef __cplusplus
--- arduino/strobe/led_array.c
+++ arduino/strobe/led_array.c
@@ -1,4 +1,5 @@
 #include "led_array.h"
+#include "color.h"
 
 static uint16_t position_to_index(uint8_t row, uint8_t col);
 
@@ -9,6 +10,12 @@ void led_window(led_strip_t *strip, window_t *window)
   for (row = window->row_start; row < window->row_end; row++) {
     for (col = window->col_start; col < window->col_end; col++) {
       uint16_t index = position_to_index(row, col);
+      color_t output_color;
+      color_t black;
+      black.r = 0;
+      black.b = 0;
+      black.g = 0;
+      lerp_color(&window->color, &black, 0.5, &output_color);
       led_set(strip, index, &window->color);
     }
   }
@@ -16,6 +23,16 @@ void led_window(led_strip_t *strip, window_t *window)
 
 static uint16_t position_to_index(uint8_t row, uint8_t col)
 {
-  if(row % 2 == 0) return row * COL_COUNT + col;  
-  return (1 + row) * COL_COUNT - col - 1;
+  if(row > 17 || col > 29) return 0;
+ 
+  uint16_t offset;
+  if(row > 8) {
+    offset = 540 - 1;
+    row -= 9;
+  } else {
+    offset = 270 - 1
+  }
+
+  if(row % 2 == 0) return offset - (row * COL_COUNT + col);  
+  return offset - ((1 + row) * COL_COUNT - col - 1);
 }
