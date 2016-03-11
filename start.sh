1802d3d32d2daaa4e9f2c09dd25990034d909304
Brandon Mathis <bemathis@gmail.com>
2016-03-11 16:57:05 -0500

    Updates to start script and initial images

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-./bin/social >> logs/social.log 2>&1 &
+(./bin/social >> logs/social.log 2>&1) &
 echo $! > pids/social.pid
 
-lein run cycle >> logs/clojure.log 2>&1 &
+(lein run cycle --update-interval 20 >> logs/clojure.log 2>&1) &
 echo $! > pids/clojure.pid
