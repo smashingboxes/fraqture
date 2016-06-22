886a160a5e0005516031b5f71ee287782c7210d7
Kevin McAbee <kevin.mcabee@smashingboxes.com>
2016-06-22 09:22:56 -0400

    Removed extraneous calls, renamed ns-map to png-xml-ns-map.

--- src/fraqture/exif.clj
+++ src/fraqture/exif.clj
@@ -7,14 +7,14 @@
 	(:import [org.apache.commons.imaging Imaging])
 	(:use [clj-xpath.core]))
 
-(def ns-map {"x" "adobe:ns:meta/" 
+(def png-xml-ns-map {"x" "adobe:ns:meta/" 
 			"rdf" "http://www.w3.org/1999/02/22-rdf-syntax-ns#" 
 			"xmp" "http://ns.adobe.com/xap/1.0/"
 			"dc" "http://purl.org/dc/elements/1.1/"})
 
 (defn getTextFromTag [xml xmlPath]
 	"retrieve the first text node (content) from xml using the provided xmlPath"
-	(with-namespace-context ns-map
+	(with-namespace-context png-xml-ns-map
 		(first ($x:text* xmlPath xml))))
 
 (defn xmp-xml [filename]
@@ -22,13 +22,10 @@
 	(let [file (java.io.File. filename)
 		  metadata (Imaging/getXmpXml file)]
 		  (if ((complement nil?) metadata)
-			(with-namespace-context ns-map 
-			   {
-			   	:author (getTextFromTag metadata "//dc:creator/*/rdf:li")
-			    :title (getTextFromTag metadata "//dc:title/*/rdf:li")
-			    :description (getTextFromTag metadata "//dc:description/*/rdf:li")
-			    :date (getTextFromTag metadata "//xmp:CreateDate")
-			    })
+			{:author (getTextFromTag metadata "//dc:creator/*/rdf:li")
+			 :title (getTextFromTag metadata "//dc:title/*/rdf:li")
+			 :description (getTextFromTag metadata "//dc:description/*/rdf:li")
+			 :date (getTextFromTag metadata "//xmp:CreateDate")}
 			({ :error "No metadata found." }))))
 
 	
